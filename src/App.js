d50640ae490f8dbd1bf5ab45e0e295fe013e6fe4
Erick Melo de Moura <erick.moura@infraclearsalecom.onmicrosoft.com>
2022-05-14 19:51:20 -0300

    Creating new components and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search';
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      setIsLoading(true)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      setCharacters(result.data)
+      setIsLoading(false)
+    }
+
+    fetchCharacters()
+  }, [query])
+
+  const queryFunction = (q) => {
+    setQuery(q)
+  }
+
   return (
-    <>
-      Hello world
-    </>
+    <div className='container'>
+      <Header />
+      <Search getQuery={queryFunction} />
+      <CharacterGrid isLoading={isLoading} items={characters} />
+    </div>
   );
 }
 
